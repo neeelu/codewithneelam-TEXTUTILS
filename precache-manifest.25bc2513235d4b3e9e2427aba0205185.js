735ab91426c233cfd62d02821f9d4c6469ae41c1
neeelu <neelampansuria@gmail.com>
2022-07-27 21:10:01 +0530

    Updates

---
+++ precache-manifest.25bc2513235d4b3e9e2427aba0205185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1cf6597a52e111546a8",
+    "url": "/codewithneelam-TEXTUTILS/static/css/main.6cb918ac.chunk.css"
+  },
+  {
+    "revision": "f1cf6597a52e111546a8",
+    "url": "/codewithneelam-TEXTUTILS/static/js/main.f1cf6597.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/codewithneelam-TEXTUTILS/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/codewithneelam-TEXTUTILS/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "029ad7d7860d54b43375",
+    "url": "/codewithneelam-TEXTUTILS/static/js/runtime~main.029ad7d7.js"
+  },
+  {
+    "revision": "4958712b8f9c2be763e267a5b2a2f9be",
+    "url": "/codewithneelam-TEXTUTILS/index.html"
+  }
+];
